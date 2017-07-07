f4265381f70c73d7cdb93192fb442236f3b9f3fe
JeffDev74 <jeffdev74@gmail.com>
2017-07-07 02:35:24 -0700

    Added new routes to handle index and save settings

--- plugins/twitchbot/routes/routes.js
+++ plugins/twitchbot/routes/routes.js
@@ -10,6 +10,7 @@ define([ 'fs', '../index.js', 'path' ], function( fs, plugin, path ) {
 
   TwitchBotRoutes.prototype.GetRequest = function(req, res, next) {
     var that = this;
+
     var appLog = req.app.get('log');
     __dirname = path.resolve(path.dirname(''));
     // var urlParts = req.url.replace(/^\/|\/$/g, '').split(path.sep);
@@ -34,10 +35,98 @@ define([ 'fs', '../index.js', 'path' ], function( fs, plugin, path ) {
     //return res.redirect('/');
   };
 
+  // POST request handlers start
+  
   TwitchBotRoutes.prototype.PostRequest = function(req, res, next) {
-    console.log('[' + that.plugin.name + '] received a POST request.');
-    return res.redirect('/');
+    var that = this;
+    var urlParts = req.url.replace(/^\/|\/$/g, '').split(path.sep);
+    var action = urlParts[2]; 
+    if(action == null) {
+      that.SaveSettings(req, res, next);
+    } else {
+      return res.status(404).render('404', {
+        title: '404 Page not found'
+      });
+    }
+
+    //return res.redirect('/plugin/twitchbot');
   };
+
+  TwitchBotRoutes.prototype.SaveSettings = function(req, res, next) {
+    var that = this;
+
+    // { 
+    //   api_key: 'api_key_value',
+    //   oath_key: 'aoth_key_value',
+    //   bot_name: 'bot_name_value',
+    //   ch_name: 'channel_name_value' 
+    // }
+
+    var newSettings = {};
+    newSettings.api_key = req.body.api_key;
+    newSettings.oath_key = req.body.oath_key;
+    newSettings.bot_name = req.body.bot_name;
+    newSettings.ch_name = req.body.ch_name;
+
+
+    req.app.get('db').collection(that.plugin.collection, function(err, settings) {
+      
+      settings.save(newSettings, function(err, result) {
+        
+        var meta = meta || {};
+        var items = items || {};
+
+        req.app.get('ejs').renderFile(__dirname + '/plugins/' + that.plugin.id + '/views/settings.ejs', {
+          meta : meta,
+          items : items
+          }, function(err, html) {
+            if (!err) {
+              return res.render('override', {
+                content: html,
+                plugin: that.plugin.id,
+                title: 'Settings ' + that.plugin.name
+              });
+            } else {
+              console.log(err);
+              return res.status(500).render('500', {
+                title: '500 Internal Server Error'
+              });
+            }
+          }
+        );
+      });
+    });
+
+    // req.app.get('db').collection(that.plugin.collection, function(err, collection) {
+
+    //   console.log(req.body);
+
+    //   collection.find().toArray(function(err, items) {
+    //       var meta = meta || {};
+    //       req.app.get('ejs').renderFile(__dirname + '/plugins/' + that.plugin.id + '/views/settings.ejs', {
+    //         meta : meta,
+    //         items : items
+    //       }, function(err, html) {
+    //         if (!err) {
+    //           return res.render('override', {
+    //             content: html,
+    //             plugin: that.plugin.id,
+    //             title: 'Settings ' + that.plugin.name
+    //           });
+    //         } else {
+    //           console.log(err);
+    //           return res.status(500).render('500', {
+    //             title: '500 Internal Server Error'
+    //           });
+    //         }
+    //       }
+    //     );
+    //     });
+    // });
+  }
+
+  // POST request handlers end
+
   TwitchBotRoutes.prototype.PutRequest = function(req, res, next) {
     console.log('[' + that.plugin.name + '] received a PutRequest request.');
     return res.redirect('/');
@@ -47,6 +136,54 @@ define([ 'fs', '../index.js', 'path' ], function( fs, plugin, path ) {
     console.log('[' + that.plugin.name + '] received a DeleteRequest request.');
     return res.redirect('/');
   };
+
+  
+  // GET request handlers start
+
+  TwitchBotRoutes.prototype.GetRequestsHandler = function (req, res, next) {
+    var that = this;
+
+    var urlParts = req.url.replace(/^\/|\/$/g, '').split(path.sep);
+    var action = urlParts[2]; 
+
+    if(action == null) {
+      that.PluginIndex(req, res, next);
+    } else {
+      return res.status(404).render('404', {
+        title: '404 Page not found'
+      });
+    }
+  };
+
+  TwitchBotRoutes.prototype.PluginIndex = function(req, res, next) {
+    var that = this;
+    var meta = meta || {};
+    var items = items || {};
+    req.app.get('ejs').renderFile(__dirname + '/plugins/' + that.plugin.id + '/views/settings.ejs', {
+        meta : meta,
+        items : items
+      }, function(err, html) {
+        if (!err) {
+          return res.render('override', {
+            content: html,
+            plugin: that.plugin.id,
+            title: 'Settings ' + that.plugin.name
+          });
+        } else {
+          console.log(err);
+          return res.status(500).render('500', {
+            title: '500 Internal Server Error'
+          });
+        }
+      }
+    );
+  };
+
+  // GET request handlers end
+
+  var exports = TwitchBotRoutes;
+
+  return exports;
 
   // HTTP METHODS NOT CURRENTLY USED
   
@@ -70,42 +207,4 @@ define([ 'fs', '../index.js', 'path' ], function( fs, plugin, path ) {
   //   return res.redirect('/');
   // };
 
-
-  TwitchBotRoutes.prototype.GetRequestsHandler = function (req, res, next) {
-    var that = this;
-    req.app.get('db').collection(that.plugin.collection, function(err, collection) {
-        collection.find().toArray(function(err, items) {
-          var meta = meta || {};
-          var urlParts = req.url.replace(/^\/|\/$/g, '').split(path.sep);
-          var viewName = urlParts[2]; 
-          if(typeof viewName !== 'undefined' || viewName == "" || viewName === undefined) {
-            viewName = 'settings';
-          }
-          
-          req.app.get('ejs').renderFile(__dirname + '/plugins/' + that.plugin.id + '/views/'+viewName+'.ejs', {
-            meta : meta,
-            items : items
-          }, function(err, html) {
-            if (!err) {
-              return res.render('override', {
-                content: html,
-                plugin: that.plugin.id,
-                title: 'Settings ' + that.plugin.name
-              });
-            } else {
-              console.log(err);
-              return res.status(500).render('500', {
-                title: '500 Internal Server Error'
-              });
-            }
-          }
-        );
-        });
-      }
-    );
-  };
-
-  var exports = TwitchBotRoutes;
-
-  return exports;
 });
